4a09d11e4523055e82275f2f72f970bf4a8bce39
emmaglorypraise <emmaglorypraise@gmail.com>
2020-07-25 23:49:50 +0100

    files upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,43 @@
+// MAIN PAGE NAV LDKDHHFHFHFJDJDDHDHDHDJJJ
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// accordion script?
+$(document).ready(function(){
+  // Add minus icon for collapse element which is open by default
+  $(".collapse.show").each(function(){
+    $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+  });
+  
+  // Toggle plus minus icon on show hide of collapse element
+  $(".collapse").on('show.bs.collapse', function(){
+    $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+  }).on('hide.bs.collapse', function(){
+    $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+  });
+});
+
+//Modal script
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
